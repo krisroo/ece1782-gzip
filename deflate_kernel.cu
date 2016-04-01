0b390410376b7175cabbc6c771b9d9750003a168
krisroo <kyeongmo.kang@gmail.com>
2016-03-31 20:55:57 -0400

    Create deflate_kernel.cu
    First commit

---
+++ deflate_kernel.cu
@@ -0,0 +1,14 @@
+#ifndef _DEFLATE_KERNEL_H_
+#define _DEFLATE_KERNEL_H_
+
+#define RADIUS 4
+#define SMEM_W 128
+#define SMEM_H 128
+
+__global__ void deflatekernel(unsigned int size, unsigned int *huffman_table, char *data_in, char *data_out)
+{
+  return;
+}
+
+#endif // _DEFLATE_KERNEL_H_
+
