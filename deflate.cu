a18a9ff7dcbcce99d3689e7c919566f453c29909
whatAmIDoingWithLife <ryan-xi502@hotmail.com>
2016-04-04 23:53:48 -0400

    Hard work tomorrow
    file handle fix, simple kernel to copy file using smem and coalesced mem
    access

--- deflate.cu
+++ deflate.cu
@@ -10,13 +10,17 @@
 
 #include "deflate_kernel.cu"
 
-#define CHUNK_SIZE 32000
+// defined in deflate_kernel.cu
+// define CHUNK_SIZE 32768
+// define THREAD_NUM 1024
+
 
 // Input: Filename
 int main(int argc, char *argv[]) 
 {
    int i;
-   int f_in;
+   int f_handle;
+   char *f_in;
    char *f_out;
    struct stat finfo;  
    char * inputfname;
@@ -31,20 +35,21 @@ int main(int argc, char *argv[])
    inputfname = argv[1];
    outputfname = argv[2];
 
-   f_in = open(inputfname, O_RDONLY);
-   fstat(f_in, &finfo);
+   f_handle = open(inputfname, O_RDONLY);
+   fstat(f_handle, &finfo);
 
+   f_in = (char*) malloc(finfo.st_size);
    f_out = (char*) malloc(finfo.st_size);
    unsigned int data_bytes = (unsigned int)finfo.st_size;
    printf("This file has %d bytes data\n", data_bytes);
 
-   read (f_in, f_out, data_bytes);
+   read (f_handle, f_in, data_bytes);
    
    
 
    //Set the number of blocks and threads
    dim3 grid(1, 1, 1);
-   dim3 block(1024, 1, 1);
+   dim3 block(THREAD_NUM, 1, 1);
 
    char* d_in;
    cudaMalloc((void**) &d_in, data_bytes);
@@ -54,7 +59,25 @@ int main(int argc, char *argv[])
    cudaMalloc((void**) &d_out, data_bytes);
    cudaMemset(d_out, 0, data_bytes);
 
+   
+   struct timeval start_tv, end_tv;
+   time_t sec;
+   time_t ms;
+   time_t diff;
+   gettimeofday(&start_tv, NULL);
+
    deflatekernel<<<grid, block>>>(data_bytes, d_in, d_out);
+
+   cudaThreadSynchronize();
+
+   gettimeofday(&end_tv, NULL);
+   sec = end_tv.tv_sec - start_tv.tv_sec;
+   ms = end_tv.tv_usec - start_tv.tv_usec;
+
+   diff = sec * 1000000 + ms;
+
+   printf("%10s:\t\t%fms\n", "Time elapsed", (double)((double)diff/1000.0));
+
 
    cudaMemcpy(f_out, d_out, data_bytes, cudaMemcpyDeviceToHost);
 
--- deflate_kernel.cu
+++ deflate_kernel.cu
@@ -1,13 +1,49 @@
 #ifndef _DEFLATE_KERNEL_H_
 #define _DEFLATE_KERNEL_H_
 
-#define RADIUS 4
-#define SMEM_W 128
-#define SMEM_H 128
+#define CHUNK_SIZE 32768
+#define THREAD_NUM 1024
+
 
 __global__ void deflatekernel(unsigned int size, char *data_in, char *data_out)
 {
-  return;
+   __shared__ char sdata[CHUNK_SIZE];
+   int idx = threadIdx.x;
+   int iteration = (size + CHUNK_SIZE - 1) / CHUNK_SIZE;
+   
+   // Iterate through all of chunks
+   for (int iter = 0; iter < iteration; iter++)
+   {
+      // Load your own share of data, threads..
+      for (int i = 0; i < CHUNK_SIZE/THREAD_NUM; i++)
+      {
+         int cpByte_in = idx + i * THREAD_NUM + iter * CHUNK_SIZE;
+         int cpByte_s  = idx + i * THREAD_NUM;
+         
+         if (cpByte_in < size)
+         {
+            sdata[cpByte_s] = data_in[cpByte_in];
+         }
+      }
+      __syncthreads();
+
+      // Search algo and fill output
+
+      // temp code to straight copy to output
+      for (int i = 0; i < CHUNK_SIZE/THREAD_NUM; i++)
+      {
+         int cpByte_out = idx + i * THREAD_NUM + iter * CHUNK_SIZE;
+         int cpByte_s  = idx + i * THREAD_NUM;
+         
+         if (cpByte_out < size)
+         {
+            data_out[cpByte_out] = sdata[cpByte_s];
+         }
+      }
+      __syncthreads();
+   }
+
+   return;
 }
 
 #endif // _DEFLATE_KERNEL_H_
