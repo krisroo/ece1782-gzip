8cd681492303b33d73b1f19f04408f48b8a50ecc
krisroo <kyeongmo.kang@gmail.com>
2016-04-07 00:48:08 -0400

    Update deflate.cu
    3x4 grid compression using 32KB block size

--- deflate.cu
+++ deflate.cu
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -10,15 +11,12 @@
 
 #include "deflate_kernel.cu"
 
-// defined in deflate_kernel.cu
-// define CHUNK_SIZE 32768
-// define THREAD_NUM 1024
-
+#define CHUNK 32768
+#define THREAD_NUM 1024
 
 // Input: Filename
 int main(int argc, char *argv[]) 
 {
-   int i;
    int f_handle;
    char *f_in;
    char *f_out;
@@ -48,7 +46,7 @@ int main(int argc, char *argv[])
    
 
    //Set the number of blocks and threads
-   dim3 grid(1, 1, 1);
+   dim3 grid(12, 1, 1);
    dim3 block(THREAD_NUM, 1, 1);
 
    char* d_in;
@@ -59,6 +57,12 @@ int main(int argc, char *argv[])
    cudaMalloc((void**) &d_out, data_bytes);
    cudaMemset(d_out, 0, data_bytes);
 
+   int aligned_chunk_count = ((data_bytes + CHUNK - 1)/CHUNK) - 1;
+   printf("Number of blocks = %i\n",aligned_chunk_count);
+   unsigned int *output_size;
+   cudaMalloc((void**) &output_size, sizeof(unsigned int)*aligned_chunk_count);
+   cudaMemset(output_size, 0, sizeof(unsigned int)*aligned_chunk_count);
+
    
    struct timeval start_tv, end_tv;
    time_t sec;
@@ -66,7 +70,7 @@ int main(int argc, char *argv[])
    time_t diff;
    gettimeofday(&start_tv, NULL);
 
-   deflatekernel<<<grid, block>>>(data_bytes, d_in, d_out);
+   deflatekernel<<<grid, block>>>(data_bytes, d_in, d_out, output_size);
 
    cudaThreadSynchronize();
 
@@ -78,7 +82,18 @@ int main(int argc, char *argv[])
 
    printf("%10s:\t\t%fms\n", "Time elapsed", (double)((double)diff/1000.0));
 
-
+   unsigned int *host_output_size = (unsigned int *)malloc(sizeof(unsigned int)*aligned_chunk_count);
+   
+   cudaMemcpy(host_output_size, output_size, sizeof(unsigned int)*aligned_chunk_count, cudaMemcpyDeviceToHost);
+   
+   unsigned int total_size = 0;
+   for (int i=0; i < aligned_chunk_count; i++) {
+      printf("this chunk size is %u\n", *(host_output_size+i));
+      total_size += *(host_output_size + i);
+   }
+      
+   printf("total size was %u\n", total_size);
+   
    cudaMemcpy(f_out, d_out, data_bytes, cudaMemcpyDeviceToHost);
 
    // Inflate data_out using zlib
@@ -88,9 +103,12 @@ int main(int argc, char *argv[])
 
    FILE *writeFile;
    writeFile = fopen(outputfname,"w+");
-   for(i = 0; i < data_bytes; i++)
-      fprintf(writeFile,"%c", f_out[i]);
+   for (unsigned int j = 0; j < aligned_chunk_count; j++) {
+       for(unsigned int i = 0; i < *(host_output_size + j); i++)
+          fprintf(writeFile,"%c", f_out[i + (j*CHUNK)]);
+   }
    fclose(writeFile);
+   
 
    return 0;
 } 
